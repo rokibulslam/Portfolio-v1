45e78d350d86255a81c79c6b71b8bbdf666590ea
rokibulslam <rokibulslam6@gmail.com>
2022-12-16 22:50:51 +0600

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import Sidebar from "./Components/Sidebar/Sidebar";
 
-
+import './app.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home.jsx/Home";
+import About from "./Pages/About/About";
+import Services from "./Pages/Services/Services";
+import Contact from "./Pages/Contact/Contact";
+import Portfolio from "./Pages/Portfolio/Portfolio";
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="app">
+      <BrowserRouter>
+        <Sidebar>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/comment" element={<Services />} />
+            <Route path="/analytics" element={<Contact />} />
+            <Route path="/portfolio" element={<Portfolio /> } />
+          </Routes>
+        </Sidebar>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import {
+  FaTh,
+  FaBars,
+  FaUserAlt,
+  FaRegChartBar,
+  FaCommentAlt,
+  FaShoppingBag,
+  FaThList,
+} from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
+import "./sidebar.css";
+const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const menuItem = [
+    {
+      path: "/",
+      name: "Home",
+      icon: <FaTh />,
+    },
+    {
+      path: "/about",
+      name: "About",
+      icon: <FaUserAlt />,
+    },
+    {
+      path: "/services",
+      name: "Services",
+      icon: <FaRegChartBar />,
+    },
+    {
+      path: "/portfolio",
+      name: "Portfolio",
+      icon: <FaCommentAlt />,
+    },
+    {
+      path: "/contact",
+      name: "Contact",
+      icon: <FaShoppingBag />,
+    },
+    {
+      path: "/productList",
+      name: "Product List",
+      icon: <FaThList />,
+    },
+  ];
+  return (
+    <div className="">
+      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+        <div className="top_section">
+          <h1 className="logo" style={{ display: isOpen ? "block" : "none" }}>
+            Logo
+          </h1>
+          <div className="" style={{ marginLeft: isOpen ? "50px" : "0px" }}>
+            <FaBars onClick={toggle} />
+          </div>
+        </div>
+        {menuItem.map((item, index) => (
+          <NavLink
+            to={item.path}
+            key={index}
+            className="link"
+            activeclassName="active"
+          >
+            <div className="flex items-center">
+              <div className="icon mr-3">{item.icon}</div>
+              <div
+                style={{ display: isOpen ? "block" : "none" }}
+                className="link_text"
+              >
+                {item.name}
+              </div>
+            </div>
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
